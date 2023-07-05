24980959b4153ae560484b752f77d2664e8488b7
Reinhard Chemonges <chemorein24@gmail.com>
2023-07-05 11:18:20 +0300

    first commit

---
+++ home.php
@@ -0,0 +1,55 @@
+<link rel="stylesheet" href="admin/assets/wysiwyg/css/froala_editor.css">
+  <link rel="stylesheet" href="admin/assets/wysiwyg/css/froala_style.css">
+<?php 
+include 'admin/db_connect.php'; 
+?>
+<style>
+    #cat-list li{
+        cursor: pointer;
+    }
+       #cat-list li:hover {
+        color: white;
+        background: #007bff8f;
+    }
+    .prod-item p{
+        margin: unset;
+    }
+    .bid-tag {
+    position: absolute;
+    right: .5em;
+}
+</style>
+<?php 
+$cid = isset($_GET['category_id']) ? $_GET['category_id'] : 0;
+?>
+<div class="container">
+    <div class="col-lg-12">
+      <div class="fr-wrapper">
+          <p>Welcome to our Corruption Reporting System, where transparency and accountability are at the forefront of our mission. Our platform aims to empower individuals to make a difference by providing a secure and reliable channel for reporting and combating corruption.
+
+At our Corruption Reporting System, we believe that every citizen's voice matters. We provide a user-friendly interface that enables you to report incidents of corruption swiftly and confidentially. Whether you have witnessed bribery, embezzlement, or any other form of corrupt activity, our platform ensures that your identity remains protected throughout the reporting process.</p><br>
+
+
+<div style="display: flex; justify-content: center; align-items: center;">
+<img src="images/co2.jpg" alt="Description of the image" style="width: 350px; height: 250px;" />
+   <img src="images/co3.jpg" alt="Description of the image" style="width: 350px; height: 250px;" />
+</div><br><hr>
+
+
+
+<p>Our Corruption Reporting System offers a range of key features to empower users in the fight against corruption. With our user-friendly interface, individuals can submit reports anonymously, ensuring their identity remains protected throughout the process. The system prioritizes the security and confidentiality of submitted data, providing a secure platform for reporting incidents. We've streamlined the reporting process, making it easy to provide comprehensive details and relevant evidence.</p><br>
+
+<div style="display: flex; justify-content: center; align-items: center;">
+<img src="images/co4.jpg" alt="Description of the image" style="width: 350px; height: 250px;" />
+   <img src="images/co5.jpg" alt="Description of the image" style="width: 350px; height: 250px;" />
+</div><br><hr>
+
+<p>If found guilty of engaging in corrupt practices through our Corruption Reporting System, individuals will face significant penalties aimed at deterring such behavior and upholding the principles of justice and integrity. These penalties may include severe legal consequences, such as fines, imprisonment, or both, depending on the nature and severity of the corruption offense committed. Additionally, individuals found guilty may face reputational damage, loss of professional licenses or certifications, and exclusion from participating in government contracts or public office.</p>
+
+      </div>
+    </div>
+</div>
+       
+<script>
+    
+</script>
